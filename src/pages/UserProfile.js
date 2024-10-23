92629ecb21f3959c431e318462961b21862453ab
Tiphaine <tiphaine.aubin28@hotmail.fr>
2024-10-23 15:55:39 +0200

    Fix access to profile page

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Account from '../components/Account';
 import '../components/style/Account.css';
 import ProfileSettings from '../components/ProfileSettings';
 import '../components/style/ProfileSettings.css';
 import { accountData } from '../data/data';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 function UserProfile() {
+    const isConnected = useSelector((state) => state.user.isConnected); // Vérification de l'état de connexion
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        // Si l'utilisateur n'est pas connecté, rediriger vers la page de connexion
+        if (!isConnected) {
+            navigate('/signin');
+        }
+    }, [isConnected, navigate]);
+
     return (
         <main className="main bg-dark">
             <ProfileSettings />
